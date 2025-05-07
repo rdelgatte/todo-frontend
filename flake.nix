4a0cce7f51b13124535b0491a6fc10daa974534d
Remi Delgatte <remi.delgatte@aletiq.com>
2025-05-07 14:15:26 +0200

    initial setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Flake outputs
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_22
+              yarn
+            ];
+          };
+        };
+      }
+    );
+
+}
